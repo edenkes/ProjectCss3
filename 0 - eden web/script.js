538b6bbd4edfe230f11fc8032aa9616bb41899cf
edenk <edenkeshet@gmail.com>
2018-11-18 17:55:16 +0200

    add icon

--- 0 - eden web/script.js
+++ 0 - eden web/script.js
@@ -1,2 +1,10 @@
 // Animations init
 new WOW().init();
+
+$(document).ready(function() {
+    $("body").tooltip({ selector: '[data-toggle=tooltip]' });
+});
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
